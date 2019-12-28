0808037d8fd92f4d29791dbfcd1c8f6be82637ad
sani kumar <sanikumar21@gmail.com>
2019-12-28 18:22:14 +0530

    Update infinity-algorithm.h

--- infinity-algorithm.h
+++ infinity-algorithm.h
@@ -1,13 +1,29 @@
 class universal
 { 
   public:
-  long int a,b;
+  long int a,b,c;
   void input()
   {
   cout<<"enter any number";
   cin>>a;
   cout<<"enter any other number"
   cin>>b;
+  }
+  
+  void calculation()
+  {
+    c= a + b;
+  }
+    
+  display()
+  {
+    cout<<"results"<<c;
+  }
   
  
 }
+ int  main( universal ob1)
+ {
+   ob1.display();
+ }
+
